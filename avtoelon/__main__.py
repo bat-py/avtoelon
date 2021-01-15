04d731a91162721e1e54382df7ca3de06b6f2cf5
Behruz <behruzfox@mail.ru>
2021-01-15 18:52:04 +0500

    padx

--- avtoelon/__main__.py
+++ avtoelon/__main__.py
@@ -2,14 +2,30 @@ from tkinter import *
 import parser
 
 root = Tk()
-root.title("Парсер сайта avtoelon.uz")
+root.title("Парсер сайта tashkent.hh.uz")
 root.geometry('600x400')
+root.resizable(width=False, height=False)
 
 
-lab = Label(root, text='\n  Пожалуйста выберите тип транспорта')
-lab.grid(row=0, column=0 )
+# Первая страница (выбор по марке или по городу)
+def first_page():
+    first_windows = Frame()
+    first_windows.pack(fill=BOTH)
+    
+    lab = Label(first_windows, text='\n  Пожалуйста выберите нужные вам профессии\n', anchor=E, bg='grey')
+
+# В r нем хранится выбор пользователя
+
+# Переход на 2 страницу
+    but = Button(first_windows, text="Далее", padx=5, pady=2)
+
+# Pack system
+    lab.pack(pady=(10,300))
+    but.pack()
 
 
 
+
+first_page()
 
 root.mainloop()
