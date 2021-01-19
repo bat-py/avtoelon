41dbaa7369cf84dc29fecb72d3da1710690b6dba
behruz <bat.py@protonmail.com>
2021-01-19 10:50:36 +0500

    Added class ScrollBar

--- avtoelon/gui.py
+++ avtoelon/gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import my_parser
 
 class CheckButton:
@@ -12,6 +13,37 @@ class CheckButton:
             onvalue=self.url, offvalue="", anchor=W, bg="white")
         self.cb.pack(fill=X)
 
+
+
+class ScrollBar:
+    def __init__(self, main_frame):
+        self.main_frame = main_frame
+
+        # Create a Canvas
+        self.my_canvas = Canvas(self.main_frame)
+        self.my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+        # Add a scrollbar to the canvas
+        self.my_scrollbar = ttk.Scrollbar(self.main_frame, orient=VERTICAL, command=self.my_canvas.yview)
+        self.my_scrollbar.pack(side=RIGHT, fill=Y)
+
+        # Configure The Canvas
+        self.my_canvas.configure(yscrollcommand=self.my_scrollbar.set)
+        self.my_canvas.bind('<Configure>', lambda e: self.my_canvas.configure(scrollregion = self.my_canvas.bbox("all")))
+
+
+        # Configure mousewheel
+        def on_mousewheel(self, event):
+            self.my_canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+        self.my_canvas.bind_all("<MouseWheel>", on_mousewheel)
+
+        # Create Another Frame INSIDE the canvas
+        self.frame_in_canvas  = Frame(self.my_canvas)
+
+        # Add that New Frame to a Window in the canvas
+        self.my_canvas.create_window((0,0), window=self.frame_in_canvas, anchor=N+W)
+
+    
 
 
 
@@ -29,10 +61,12 @@ def first_page(root):
     dic_item = my_parser.list_jobs()
     list_item = dic_item.items()
 
+    frame_in_canvas = ScrollBar(inner_frame)
     
     buttons = []
     for name, url in list_item:
-        buttons.append(CheckButton(inner_frame, name, url))
+        buttons.append(CheckButton(frame_in_canvas.frame_in_canvas, name, url))
+    
 
 
 # Переход на 2 страницу
