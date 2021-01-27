edbc862953cd3530aac0c454fbd314426208be34
behruz <bat.py@protonmail.com>
2021-01-27 22:03:59 +0500

    Fixed tk.PhotoImage()

--- avtoelon/__main__.py
+++ avtoelon/__main__.py
@@ -3,6 +3,7 @@ import gui
 from ttkthemes import ThemedStyle
 from PIL import ImageTk, Image
 import os
+from tkinter import ttk
 
 class Root(tk.Tk):
     def __init__(self):
@@ -11,27 +12,26 @@ class Root(tk.Tk):
         self.title("Парсер сайта tashkent.hh.uz")
         self.geometry('600x400')
         self.resizable(width=False, height=False)
-        
+       
         path = os.path.dirname(__file__)
 #        self.iconbitmap(path+'/images/hh.ico')
 
         style = ThemedStyle(self)
         style.set_theme('breeze')
 
-        self.top_menu = tk.Frame(self, bg="white", height=65, width=600)
+        self.top_menu = tk.Frame(self)
         self.top_menu.pack()
-        self.top_menu.pack_propagate(False)
 
-        self.top_frame()
+        imag = tk.PhotoImage(file='hh.png')
+        lab = tk.Label(self, image=imag)
+        lab.pack()
+ 
         gui.FirstPage(self)
 
+
+#        my_image = ImageTk.PhotoImage(Image.open('./images/hh.png'))
+#        image_lab = tk.Label(self.top_menu, image=my_image)
+#        image_lab.pack()
+
         self.mainloop()
-
-    def top_frame(self):
-        my_image = ImageTk.PhotoImage(Image.open('./images/hh.jpg'))
-        image_lab = tk.Label(self.top_menu, image=my_image)
-        image_lab.pack()
-#        image = tk.PhotoImage(file='images/hh.png')
-#        lab = tk.Label(self.top_menu, image=image, text='Helo').pack()
-
 Root()
