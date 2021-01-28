cc313e3b2586661688bab41bc10839567b18fffb
behruz <bat.py@protonmail.com>
2021-01-28 10:00:38 +0500

    added title

--- avtoelon/__main__.py
+++ avtoelon/__main__.py
@@ -19,20 +19,24 @@ class Root(tk.Tk):
         style = ThemedStyle(self)
         style.set_theme('breeze')
 
+
+        # Top Frame settings
         self.top_menu = tk.Frame(self, bg="white", height=65, pady=5)
         self.top_menu.pack(fill=tk.X)
         self.top_menu.pack_propagate(False)
-
         
+        # Image In the LEFT side of Frame
         my_image = Image.open('images/hh.png')
         my_image = my_image.resize((53, 53), Image.ANTIALIAS)
-
         image = ImageTk.PhotoImage(my_image)
         
         image_lab = tk.Label(self.top_menu, image=image)
         image_lab.pack(side=tk.LEFT, padx=5)
 
- 
+        about_program = 'С помощью этой программы вы можете скачать объявления из сайта tashkent.hh.uz\nОбъявления можно сохранить в xlsx (Excel) или в csv формате'
+        doc_label = tk.Label(self.top_menu, text=about_program)
+        doc_label.pack(side=tk.RIGHT)
+        
         gui.FirstPage(self)
 
 
